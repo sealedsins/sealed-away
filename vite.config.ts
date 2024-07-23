6d0da44db93fdc8f970f947a92da474f8f98e890
Sealed Sins <sealedsins@gmail.com>
2024-07-23 18:15:54 +0300

    Added image optimizations for production build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import ViteVue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-	plugins: [vue()],
 	publicDir: false,
+	plugins: [ViteVue(), ViteImageOptimizer()],
 	server: {
 		port: 8080,
 		fs: {
